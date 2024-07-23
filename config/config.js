452baca7c781e651b4fd5bf7df58e95039c5a341
amonani18 <165120116+amonani18@users.noreply.github.com>
2024-07-22 21:10:09 -0400

    Localserver removed changed to Power-Canada Database on Mongodb

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const config = {
   env: process.env.NODE_ENV || "development",
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-  mongoUri: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/powercanada"
+  mongoUri: process.env.MONGODB_URI || "mongodb+srv://powercan:powercan@power-cananda.9d84td9.mongodb.net/Power-Can?retryWrites=true&w=majority&appName=Power-Cananda"
 };
 
 export default config;
