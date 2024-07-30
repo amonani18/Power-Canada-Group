0b6ea8f2d3fdc74240feebcc5cf937a3ec0e74ef
amonani18 <165120116+amonani18@users.noreply.github.com>
2024-07-30 03:24:01 -0400

    Remove all files from the repository

--- server/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import authCtrl from "../controllers/auth.controller.js";
-
-const router = express.Router();
-
-router.route("/signin").post(authCtrl.signin);
-router.route("/signout").get(authCtrl.signout);
-
-export default router;
