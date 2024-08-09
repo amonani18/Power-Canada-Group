130b869a213bf530d435dadb4cb4edd0f3e05e2a
amonani18 <165120116+amonani18@users.noreply.github.com>
2024-08-08 20:09:16 -0400

    Update Dashboard.jsx

--- power-can/src/components/Dashboard.jsx
+++ power-can/src/components/Dashboard.jsx
@@ -26,7 +26,7 @@ function Dashboard() {
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await fetch('http://localhost:5000/api/users');
+        const response = await fetch('https://power-canada-group-backend.onrender.com/api/users');
         const data = await response.json();
         setUsersData(data);
       } catch (error) {
